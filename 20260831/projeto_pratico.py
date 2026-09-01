3d2c8ec9db2d381aa66179b2d6433addba13185f
CordeiroGusta <guscordeiro199@gmail.com>
2026-08-31 22:14:32 -0300

    feat: projeto finalizado

--- 20260831/projeto_pratico.py
+++ 20260831/projeto_pratico.py
@@ -1,20 +1,47 @@
 class ContaBancaria:
-    # def __init__(self, titular, cpf, numero_conta, agencia, saldo, extrato):
     def __init__(self, titular, cpf, numero_conta, agencia, saldo):
-        self.titular = titular,
-        self.cpf = cpf,
-        self.numero_conta = numero_conta,
-        self.agencia = agencia,
+        self.titular = titular
+        self.cpf = cpf
+        self.numero_conta = numero_conta
+        self.agencia = agencia
         self.saldo = saldo
-        # self.extrato = extrato
 
     def depositar(self, valor):
+        if valor <= 0:
+            return "Digite um valor maior que zero"
+        
         self.saldo = self.saldo + valor
         return self.saldo
 
-    # def saque(self, valor):
-    #     self.saldo
+    def sacar(self, valor):
+        if self.saldo < valor:
+            return "Saldo invalido"
 
-teste = ContaBancaria('Portes', 32156498701, 100000115, 3, 150)
-print(teste.saldo)
-print(f"Com mais 10, o saldo fica: {teste.depositar(50)}")
+        self.saldo = self.saldo - valor
+        return self.saldo
+
+    def mostrar_saldo(self):
+        return self.saldo
+
+    def efetuar_transacao(self, valor, chave_destinatario):
+        if self.saldo < valor:
+            return "Saldo invalido"
+
+        self.saldo = self.saldo - valor
+        return f'transferencia realizada de {valor} para a conta {chave_destinatario}\nSaldo atual: {self.mostrar_saldo()}'
+
+
+fulano = ContaBancaria('Fulano', 32165498701, 7223, 2, 2500)
+
+print("\nDados da Conta")
+print(f"Titular: {fulano.titular}")
+print(f"CPF: {fulano.cpf}")
+print(f"Numero de Conta: {fulano.numero_conta}")
+print(f"Agência: {fulano.agencia}")
+print(f"Saldo: {fulano.saldo}")
+
+print("\nMetodos:")
+print(f"Deposito de 500 na conta: {fulano.depositar(500)}")
+print(f"Saque de 200: {fulano.sacar(200)}")
+print(f"Mostrar o saldo da conta: {fulano.mostrar_saldo()}")
+print(f"Transação para uma conta: {fulano.efetuar_transacao(300, 78945612387)}")
